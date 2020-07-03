737d71f5ae0a968b2f496ce7d102519e01ab1320
Alfie Abdul-Rahman <alfie.abdulrahman@gmail.com>
2020-07-03 17:00:42 +0100

    Added Overview Top Level Screen A

--- app/home/routes.py
+++ app/home/routes.py
@@ -2,9 +2,11 @@ from app.home import blueprint
 from flask import render_template, redirect, url_for
 from jinja2 import TemplateNotFound
 
+
 @blueprint.route('/dashboard')
 def index():
     return render_template('dashboard.html')
+
 
 @blueprint.route('/<page>')
 def route_template(page):
@@ -14,5 +16,17 @@ def route_template(page):
     except TemplateNotFound:
         return render_template('page-404.html'), 404
     
+    except:
+        return render_template('page-500.html'), 500
+
+
+@blueprint.route('/overviews/<page>')
+def route_template_overview(page):
+    try:
+        return render_template('overviews/' + page + '.html')
+
+    except TemplateNotFound:
+        return render_template('page-404.html'), 404
+
     except:
         return render_template('page-500.html'), 500
