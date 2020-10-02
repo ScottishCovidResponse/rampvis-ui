4681a86d394f8bd6ba38870d69f6de9f1c8753b4
Saiful Khan <saiful.etc@gmail.com>
2020-10-02 15:43:55 +0100

    using prod api/data

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ export FLASK_APP=run.py
 # Set up the DEBUG environment
 export FLASK_ENV=development
 
-export DATA_API='http://localhost:2000/api/v1'
-export STAT_API='http://localhost:3000/stat/v1'
+export DATA_API='http://vis.scrc.uk/api/v1'
+export STAT_API='http://vis.scrc.uk/stat/v1'
 
 # MongoDB link is deleted for security reason, you can use your local database
 export MONGODB_URL='<set_the_database_url>'
