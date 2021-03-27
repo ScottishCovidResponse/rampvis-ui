4613c7d78d8db693b555c4903a03778c3f48b409
Saiful Khan <saiful.etc@gmail.com>
2021-03-27 11:32:46 +0000

    update table

--- run-use-prod-api.sh
+++ run-use-prod-api.sh
@@ -9,8 +9,6 @@ export API_PY='http://vis.scrc.uk/stat/v1'
 export DATA_API='http://vis.scrc.uk/api/v1'
 export STAT_API='http://vis.scrc.uk/stat/v1'
 
-# MongoDB link is deleted for security reason, you can use your local database
-export MONGODB_URL='<set_the_database_url>'
 
 # Start the application (development mode)
 flask run --host=0.0.0.0 --port=5000
