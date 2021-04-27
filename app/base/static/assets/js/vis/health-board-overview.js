78a06e7b2591320cea5a2620a95e77e130f54d8e
bbach <bbach@exseed.ed.ac.uk>
2021-04-27 12:55:15 +0100

    adding dashboard.js

--- app/base/static/assets/js/vis/health-board-overview.js
+++ app/base/static/assets/js/vis/health-board-overview.js
@@ -36,7 +36,6 @@ var DATASTREAM_3 = "Daily Tests";
 var DATASTREAM_4 = "Covid19 related deaths (weekly)";
 var DATASTREAM_5 = "'All Deaths (weekly)";
 
-
 class HealthBoardOverview {
     constructor(options) 
     {
@@ -82,8 +81,7 @@ var createDashboardLayout = function(div)
 
 var visualizeAllStreams = function(data)
 {
-
-    visualizeDataStream(
+    dashboard.visualizeDataStream(
         "#board-hospital-normalized",
         DATASTREAM_0,
         nhsBoardField,
@@ -92,7 +90,7 @@ var visualizeAllStreams = function(data)
         MODE_CURRENT,
         true);
 
-    visualizeDataStream(
+    dashboard.visualizeDataStream(
         "#board-hospital",
         DATASTREAM_1,
         nhsBoardField,
@@ -100,7 +98,7 @@ var visualizeAllStreams = function(data)
         data[1].values,
         MODE_CURRENT);
 
-    visualizeDataStream(
+    dashboard.visualizeDataStream(
         "#board-covid-icu",
         DATASTREAM_2,
         nhsBoardField,
@@ -108,15 +106,15 @@ var visualizeAllStreams = function(data)
         data[2].values,
         MODE_CURRENT);
 
-    visualizeDataStream(
+    dashboard.visualizeDataStream(
         "#tests",
         DATASTREAM_3,
         nhsBoardField,
         COLOR_TESTS,
         data[3].values,
         MODE_DAILY);
-
-    visualizeDataStream(
+        
+    dashboard.visualizeDataStream(
         "#covid-deaths",
         DATASTREAM_4,
         nhsBoardField,
@@ -124,317 +122,11 @@ var visualizeAllStreams = function(data)
         data[4].values,
         MODE_WEEKLY);
 
-    visualizeDataStream(
+    dashboard.visualizeDataStream(
         "#all-deaths",
         DATASTREAM_5,
         nhsBoardField,
         d3.rgb(COLOR_DEATHS).darker(.5),
         data[5].values,
         MODE_WEEKLY);
-}
-
-
-// visualizes a dataset for a dashboard with number, trend, and chart
-var visualizeDataStream = function (id, title, field, color, dataStream, mode, normalized) {
-    
-    var svg = d3.select(id)
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-
-    setVisTitle(svg, title)
-    visualizeNumber(svg, dataStream, 0, field, color, mode, normalized)
-    visualizeTrendArrow(svg, dataStream, 250, field, color, mode)
-    visualizeMiniChart(svg, dataStream, 400, field, color, mode);
-}
-
-var visualizeNumber = function (svg, data, xOffset, field, color, mode, normalized) {
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + xOffset + ",0)")
-
-    if (mode == MODE_DAILY) {
-        setVisLabel(g, 'Today')
-    } else if (mode == MODE_CURRENT) {
-        setVisLabel(g, 'Current')
-    } else if (mode == MODE_WEEKLY) {
-        setVisLabel(g, 'This week')
-    } else {
-        setVisLabel(g, 'Total')
-    }
-
-    var val = Math.round(data[data.length - 1][field] * 10) / 10;
-    val = val.toLocaleString(
-        undefined)
-
-    var bigNumber = {}
-    var t = g.append('text')
-        .text(val)
-        .attr('y', top_content + 40)
-        .attr('class', 'bigNumber')
-        .style('fill', color)
-        .each(function () {
-            bigNumber.width = this.getBBox().width;
-        })
-
-
-    if (normalized) {
-        g.append('text')
-            .text('per')
-            .attr('x', bigNumber.width + 10)
-            .attr('y', top_content + LINE_1)
-            .attr('class', 'thin')
-        g.append('text')
-            .text('100,000')
-            .attr('x', bigNumber.width + 10)
-            .attr('y', top_content + LINE_2)
-            .attr('class', 'thin')
-    } 
-    // else {
-    //     // show rank
-    //     var values = data[data.length - 1];
-    //     var array = []
-    //     for (var v in values) {
-    //         array.push([v, values[v]])
-    //     }
-    //     array.sort(function (a, b) {
-    //         return a[1] - b[1];
-    //     })
-
-    //     var rank;
-    //     for (var i = 1; i < array.length - 1; i++) {
-    //         if (array[i][0] == field) {
-    //             rank = i;
-    //             break;
-    //         }
-    //     }
-
-
-    //     if (rank) {
-    //         g.append('text')
-    //             .text(function () {
-    //                 if (rank == 1) return '1st';
-    //                 else if (rank == 2) return '2nd';
-    //                 else if (rank == 3) return '3rd';
-    //                 else return rank + 'th';
-    //             })
-    //             .attr('x', bigNumber.width + 10)
-    //             .attr('y', top_content + LINE_1)
-    //             .attr('class', 'thin')
-    //         g.append('text')
-    //             .text('Scotland')
-    //             .attr('x', bigNumber.width + 10)
-    //             .attr('y', top_content + LINE_2)
-    //             .attr('class', 'thin')
-    //     }
-    // }
-
-}
-
-
-
-var visualizeTrendArrow = function (svg, data, xOffset, field, color, mode) {
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + xOffset + ",0)")
-
-    if (mode == MODE_WEEKLY)
-        setVisLabel(g, "From last week")
-    else
-        setVisLabel(g, "From yesterday")
-
-    var secondLast = parseInt(data[data.length - 2][field])
-    var last = parseInt(data[data.length - 1][field])
-    v = last - secondLast;
-    r = 0
-    if (v < 0) r = 45;
-    if (v > 0) r = -45;
-
-    g.append('text')
-        .text(function () {
-            if (v > 0) {
-                return 'up by';
-            } else
-                if (v < 0) {
-                    return 'down by';
-                } else {
-                    return 'no ';
-                }
-        })
-        .attr('x', 45)
-        .attr('y', top_content + LINE_1)
-        .attr('class', 'thin')
-
-    if (v == 0) {
-        g.append('text')
-            .text('change')
-            .attr('x', 45)
-            .attr('y', top_content + LINE_2)
-            .attr('class', 'thin')
-    } else {
-        g.append('text')
-            .text(Math.abs(v))
-            .attr('x', 45)
-            .attr('y', top_content + LINE_2)
-            .style('fill', color)
-    }
-
-
-    var g2 = g.append('g')
-        .attr('transform',
-            function () {
-                return 'translate(17,' + (top_content + 22) + '),rotate(' + r + ')';
-            }
-        );
-
-    g2.append('line')
-        .attr('x1', -15)
-        .attr('x2', 15)
-        .attr('y1', 0)
-        .attr('y2', 0)
-        .attr('class', 'arrow')
-        .attr('stroke', color);
-    g2.append('line')
-        .attr('x1', 15)
-        .attr('x2', 0)
-        .attr('y1', 0)
-        .attr('y2', -15)
-        .attr('class', 'arrow')
-        .attr('stroke', color);
-    g2.append('line')
-        .attr('x1', 15)
-        .attr('x2', 0)
-        .attr('y1', 0)
-        .attr('y2', 15)
-        .attr('class', 'arrow')
-        .attr('stroke', color);
-
-}
-
-
-var visualizeMiniChart = function (svg, data, xOffset, field, color, mode) {
-
-    var chartWidth = 100;
-    var chartHeight = 35;
-    var trendWindow = 14 // days
-    if (mode == MODE_WEEKLY)
-        trendWindow = 8
-
-    var barWidth = (chartWidth - 10) / trendWindow;
-
-
-    var gg = svg.append('g')
-        .attr("transform", "translate(" + xOffset + ",0)")
-
-    if (mode == MODE_WEEKLY) {
-        setVisLabel(gg, 'Last ' + trendWindow + ' Weeks ')
-    } else {
-        setVisLabel(gg, 'Last ' + trendWindow + ' Days')
-    }
-
-    var g = gg.append("g")
-        .attr("transform", "translate(0," + (top_content + 5) + ")")
-
-
-    var x = d3.scaleLinear()
-        .domain([0, trendWindow - 1])
-        .range([0, chartWidth - barWidth])
-
-    // get 7 last entries
-    data = data.slice(data.length - trendWindow)
-
-    var max = d3.max(data, function (d) {
-        return parseInt(d[field]);
-    })
-    var y = d3.scaleLinear()
-        .domain([0, max])
-        .range([chartHeight, 0]);
-
-    if (mode == MODE_CUMULATIVE
-        || mode == MODE_CURRENT) {
-        g.append("path")
-            .datum(data)
-            .attr("fill", color)
-            .style('opacity', .4)
-            .attr("d", d3.area()
-                .x(function (d, i) { 
-                    return x(i) })
-                .y0(35)
-                .y1(function (d) { 
-                    return y(d[field]); })
-            )
-
-        g.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", color)
-            .attr("stroke-width", 2)
-            .attr("d", d3.line()
-                .x(function (d, i) { return x(i) })
-                .y(function (d) { return y(d[field]); })
-            )
-
-        g.append("circle")
-            .attr("fill", color)
-            .attr("r", 3)
-            .attr("cx", x(data.length - 1))
-            .attr("cy", y(data[data.length - 1][field]))
-    } else {
-        g.selectAll("bar")
-            .data(data)
-            .enter().append("rect")
-            .style("fill", function (d, i) {
-                var c = color;
-                if (i == 13)
-                    c = d3.rgb(c).darker(1)
-                return c;
-            })
-            .attr("x", function (d, i) {
-                return x(i);
-            })
-            .attr("width", barWidth)
-            .attr("y", function (d) { return y(d[field]); })
-            .attr("height", function (d) { return chartHeight - y(d[field]); });
-
-    }
-
-    if (mode == MODE_DAILY
-        || mode == MODE_CUMULATIVE
-        || mode == MODE_CURRENT) {
-        g.append('line')
-            .attr('x1', x(6.9))
-            .attr('x2', x(7.1))
-            .attr('y1', 37)
-            .attr('y2', 37)
-            .attr('class', 'weekBar')
-    }
-
-}
-
-var setVisTitle = function (g, text) {
-    g.append('line')
-        .attr('x1', 0)
-        .attr('x2', 10000)
-        .attr('y1', baseline_title + 7)
-        .attr('y2', baseline_title + 7)
-        .attr('class', 'separator')
-
-    g.append('text')
-        .text(text)
-        .attr('class', 'title')
-        .attr('y', baseline_title)
-}
-
-var setVisLabel = function (g, text) {
-    g.append('text')
-        .text(text)
-        .attr('class', 'label')
-        .attr('y', baseline_label)
-}
-
-var setVisLabelRow2 = function (g, text) {
-    g.append('text')
-        .text(text)
-        .attr('class', 'label')
-        .attr('y', baseline_label + 15)
 }
