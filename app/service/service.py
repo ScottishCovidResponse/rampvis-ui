06e83b8e450d9a2089039fe8b562295163cdafe7
Saiful Khan <saiful.etc@gmail.com>
2021-07-18 23:37:50 +0100

    #51 Create a template to implement timeseries similarity search page

--- app/service/service.py
+++ app/service/service.py
@@ -9,9 +9,10 @@ from app import app
 
 from .title import generate_title
 
-API_JS = os.environ.get('API_JS')
-API_PY = os.environ.get('API_PY')
-UI_URL = os.environ.get('UI_URL')
+API_JS = os.environ.get("API_JS")
+API_PY = os.environ.get("API_PY")
+UI_URL = os.environ.get("UI_URL")
+
 
 def get_api_url(key):
     """
@@ -19,6 +20,7 @@ def get_api_url(key):
     """
     logger.debug(key)
     return os.environ.get(key)
+
 
 def get_ui_url(key):
     """
@@ -29,7 +31,7 @@ def get_ui_url(key):
 
 
 def github_login():
-    return redirect(API_JS + '/auth/github-login')
+    return redirect(API_JS + "/auth/github-login")
 
 
 def get_user(token):
@@ -38,41 +40,41 @@ def get_user(token):
     else:
         return None
 
-    user_id = decoded_token.get('id')
-    headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' + token}
-    response = requests.get(API_JS + '/user/' + user_id, headers=headers)
+    user_id = decoded_token.get("id")
+    headers = {"content-type": "application/json", "Authorization": "Bearer " + token}
+    response = requests.get(API_JS + "/user/" + user_id, headers=headers)
 
     user = json.loads(response.content)
-    logger.debug('service: get_user: user = ', user)
+    logger.debug("service: get_user: user = ", user)
     return user
 
 
 def search(query):
-    response = requests.get(API_JS + '/scotland/search/?query=' + query)
+    response = requests.get(API_JS + "/scotland/search/?query=" + query)
     result = json.loads(response.content)
-    logger.debug('service: search: query = ', query, '\nresult = ', result)
+    logger.debug("service: search: query = ", query, "\nresult = ", result)
     return result
 
 
 def get_bookmarks():
-    token = session['token']
+    token = session["token"]
     # print('get_bookmarks: session[token] = ', token)
     if not token:
         return None
 
-    headers = {'content-type': 'application/json', 'Authorization': 'Bearer ' + token}
-    response = requests.get(API_JS + '/bookmark/', headers=headers)
+    headers = {"content-type": "application/json", "Authorization": "Bearer " + token}
+    response = requests.get(API_JS + "/bookmark/", headers=headers)
     bookmarks = json.loads(response.content)
 
-    logger.debug('get_bookmarks: bookmarks = ', bookmarks)
+    logger.debug("get_bookmarks: bookmarks = ", bookmarks)
 
     result = []
     for d in bookmarks:
-        page_id = d.get('pageId')
-        thumbnail = d.setdefault('thumbnail', "abc")
+        page_id = d.get("pageId")
+        thumbnail = d.setdefault("thumbnail", "abc")
         # print('service: get_bookmarks: d =', d, 'page_id = ', page_id)
         page_data_from_ontology = ontology.get_page_by_id(int(page_id))
-        page_data_from_ontology.get('page')['thumbnail'] = thumbnail
+        page_data_from_ontology.get("page")["thumbnail"] = thumbnail
 
         result.append(page_data_from_ontology)
 
@@ -81,13 +83,14 @@ def get_bookmarks():
 
 
 def is_bookmarked(page_id):
-    logger.debug('service: is_bookmarked: page_id = ', page_id)
+    logger.debug("service: is_bookmarked: page_id = ", page_id)
     return True
 
 
 def update_bookmark(page_id):
-    logger.debug('service: update_bookmark: page_id = ', page_id)
+    logger.debug("service: update_bookmark: page_id = ", page_id)
     return True
+
 
 #
 # call backend ontology
@@ -95,13 +98,13 @@ def update_bookmark(page_id):
 
 
 def get_onto_pages(binding_type):
-    logger.debug(f'service.py:get_onto_pages:  API_JS = {API_JS}')
+    logger.debug(f"service.py:get_onto_pages:  API_JS = {API_JS}")
 
-    response = requests.get(API_JS + '/template/pages/?filterPageType=' + binding_type)
+    response = requests.get(API_JS + "/template/pages/?filterPageType=" + binding_type)
     onto_pages = json.loads(response.content)
-    data = onto_pages.get('data', [])
+    data = onto_pages.get("data", [])
 
-    logger.debug(f'service.py: get_onto_pages: onto_pages={data}')
+    logger.debug(f"service.py: get_onto_pages: onto_pages={data}")
     return data
 
 
@@ -109,34 +112,52 @@ def get_onto_page_by_id(id):
     # logger.debug(f'service.py:get_onto_page_by_id: id = {id}')
     # logger.debug(f'\n\n..............................................')
 
-    response = requests.get(API_JS + '/template/page/' + id)
+    response = requests.get(API_JS + "/template/page/" + id)
     onto_page = json.loads(response.content)
 
-    bindings = onto_page.get('bindings')
-    bindingExts = onto_page.get('bindingExts')
+    bindings = onto_page.get("bindings")
+    bindingExts = onto_page.get("bindingExts")
     # logger.debug(f'service.py:get_onto_page_by_id: bindings = {bindings}')
     # logger.debug(f'service.py:get_onto_page_by_id: bindingExts = {bindingExts}')
 
     if bindings is None or bindingExts is None:
         return None
 
-    data = bindingExts[0].get('data')
-    vis = bindingExts[0].get('vis')
-    links = bindings[0].get('pageIds')
-    data = [{**d, 'endpoint': get_api_url(d.get('urlCode')) + d.get('endpoint')} for d in data]
+    data = bindingExts[0].get("data")
+    vis = bindingExts[0].get("vis")
+    links = bindings[0].get("pageIds")
+    data = [
+        {**d, "endpoint": get_api_url(d.get("urlCode")) + d.get("endpoint")}
+        for d in data
+    ]
 
     if links is not None:
         links = [f'{get_ui_url("UI_URL")}/{l}' for l in links]
 
-    onto_page['bindings'] = {'data': data, 'vis': vis, 'links': links}
+    onto_page["bindings"] = {"data": data, "vis": vis, "links": links}
 
     # Extract title
-    keywords_list = [d['keywords'] for d in data]
-    onto_page['title'] = generate_title(keywords_list)
+    keywords_list = [d["keywords"] for d in data]
+    onto_page["title"] = generate_title(keywords_list)
 
     # Update stream description
     for d in data:
-        d['description'] = generate_title([d['keywords']])
+        d["description"] = generate_title([d["keywords"]])
     # logger.debug(f'service.py:get_onto_page_by_id: onto_page = {onto_page}')
 
     return onto_page
+
+
+def timeseries_sim_search(query):
+    result = None
+    logger.debug(f"service:timeseries_sim_search: query = {query}")
+
+    try:
+        response = requests.get(f"{API_PY}/timeseries-sim-search?q1={query}&q2=todo")
+        result = json.loads(response.content)
+        logger.debug(f"service:timeseries_sim_search: result = {result}")
+
+    except Exception as e:
+        logger.error(f"service:timeseries_sim_search: error = {e}")
+
+    return result
