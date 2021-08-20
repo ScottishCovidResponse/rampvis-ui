407f49c718c5981ad9fac0191e57c32ab77409ed
Saiful Khan <saiful.etc@gmail.com>
2021-08-20 18:54:25 +0100

    remove jinja code

--- app/base/static/assets/js/vis/pv.js
+++
@@ -1,2 +0,0 @@
-// Namespace for phong's vises.
-pv = {};
--- app/home/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint(
-    'home_blueprint',
-    __name__,
-    url_prefix='',
-    template_folder='pages',
-    static_folder='static'
-)
