2784afd904587a306df11cbaa63ba50fbbf1e492
Saiful Khan <saiful.etc@gmail.com>
2021-08-31 17:05:28 +0100

    update default route

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       return [
           {
               source: '/',
-              destination: '/development/example',
+              destination: '/search',
               permanent: false,
           }
       ]
