c1e97a84c267c3890cab926adba0e82c810d3b99
phongvis <phonghainguyen84@gmail.com>
2021-08-31 18:42:58 +0100

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,11 +9,11 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href="/img/favicon.png" />
+          <link rel="shortcut icon" href="/Favicon32x32.png" />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/img/apple-icon.png"
+            href="/Favicon64x64.png"
           />
           {/* Fonts and icons */}
           <link
