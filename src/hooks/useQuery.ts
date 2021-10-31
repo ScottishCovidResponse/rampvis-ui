2b000fe238b702130b997b5687e55010e380d851
saiful.khan <saiful.etc@gmail.com>
2021-10-31 21:27:27 +0000

    fix eslint version and lint:tsc issues

--- src/hooks/useQuery.ts
+++
@@ -1,8 +0,0 @@
-// A custom hook that builds on useLocation to parse the query string for you.
-
-import { useLocation } from "react-router-dom";
-
-const useQuery = (): URLSearchParams =>
-  new URLSearchParams(useLocation().search);
-
-export default useQuery;
