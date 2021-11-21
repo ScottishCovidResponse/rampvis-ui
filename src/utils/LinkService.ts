ac002be6dee408fd08d96673880d41ee972bf7b9
saiful.khan <saiful.etc@gmail.com>
2021-11-21 10:35:13 +0000

    service for links and minor refactoring

---
+++ src/utils/LinkService.ts
@@ -0,0 +1,15 @@
+import { apiService } from "src/utils/ApiService";
+
+const api: string = `${process.env.NEXT_PUBLIC_API_JS}/template/link`;
+
+export async function getLinks(dataId: string) {
+  try {
+    const pageIds = await apiService.get(`${api}/${dataId}`);
+    const links = pageIds?.map((id: string) => {
+      return `page?id=${id}`;
+    });
+    return links;
+  } catch (error) {
+    console.error(`LinkService:getLinks: error = ${error}`);
+  }
+}
