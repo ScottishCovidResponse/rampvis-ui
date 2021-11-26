4ed2d009068977c998004068783dff1ad5e6b312
tunagonen <tunagonen@gmail.com>
2021-11-26 16:27:46 +0000

    timeseries v1 with segmented/full plots

---
+++ src/components/timeseries-sim/GraphTitle.tsx
@@ -0,0 +1,13 @@
+import { CardHeader } from "@mui/material";
+function GraphTitle() {
+  return (
+    <CardHeader
+      id="chartTitle"
+      sx={{
+        textAlign: "center",
+      }}
+    />
+  );
+}
+
+export default GraphTitle;
--- src/components/timeseries-sim/variables/variables.js
+++ src/components/timeseries-sim/variables/variables.js
@@ -127,3 +127,4 @@ export const continents = [
     value: "South America",
   },
 ];
+
