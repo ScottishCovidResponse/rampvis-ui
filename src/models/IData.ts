0a0db1a86517d85c6fd17bcfd8b2698e07736d7a
saiful.khan <saiful.etc@gmail.com>
2021-11-26 14:14:11 +0000

    fetch links

---
+++ src/models/IData.ts
@@ -0,0 +1,6 @@
+export interface IData {
+  id: string;
+  endpoint: string;
+  values: any;
+  description: string;
+}
---
+++ src/models/ILink.ts
@@ -0,0 +1,5 @@
+export interface ILink {
+  visFunction: string;
+  pageId?: string;
+  url?: string;
+}
