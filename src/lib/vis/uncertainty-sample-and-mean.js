a53fc74ba3f5ac01d3622e7c29c5ef1e79a823a3
Erik <supersamst.rydow@gmail.com>
2021-11-29 11:24:44 +0000

    Removed Lint Silencing

--- src/lib/vis/uncertainty-sample-and-mean.js
+++ src/lib/vis/uncertainty-sample-and-mean.js
@@ -1,14 +1,3 @@
-/* eslint-disable spaced-comment */
-/* eslint-disable one-var */
-/* eslint-disable prefer-arrow-callback */
-/* eslint-disable prefer-const */
-/* eslint-disable func-names */
-/* eslint-disable prefer-template */
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable prefer-spread */
-/* eslint-disable @typescript-eslint/lines-between-class-members */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import * as d3 from "d3";
 import Common from "./common";
 
