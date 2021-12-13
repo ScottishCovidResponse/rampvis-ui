fd5711e41f3e6c09709aee7fd876eecf63a0f512
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-13 01:44:54 +0000

    Skip linting during `yarn build` to avoid duplicate CI work

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+
+  // We call linters in GitHub Actions for all pull requests. By not linting
+  // again during `next build`, we save CI minutes and unlock more feedback.
+  // For local checks, run `yarn lint`.
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
