65a324cdb1433486598caf001025d459ba26b5bc
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-13 01:46:01 +0000

    Tweak comment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   // We call linters in GitHub Actions for all pull requests. By not linting
   // again during `next build`, we save CI minutes and unlock more feedback.
-  // For local checks, run `yarn lint`.
+  // For local checks, run `yarn lint:[name of linter]`.
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
