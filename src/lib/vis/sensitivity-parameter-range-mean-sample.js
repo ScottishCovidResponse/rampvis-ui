a26ecf735d1b3a2d76d4d0b5d1b0a743603d00d2
Erik <supersamst.rydow@gmail.com>
2022-01-12 09:45:46 +0000

    Cluster-based Sensitivity Plots

---
+++ src/lib/vis/sensitivity-parameter-range-mean-sample.js
@@ -0,0 +1,72 @@
+import * as d3 from "d3";
+import { visFactory } from "src/lib/vis/vis-factory";
+
+export class SensitivityParameterRangeSampleMean {
+
+  constructor(options) {
+    const data = options.data[0].values
+    let colors = ["#E69F00", "#009E73", "#DB4D56", "#0072B2", "#D55E00", "#F0E442", "#56B4E9"];
+    const baseColor = ["#4f4f4f"];
+    const textColor = ["#4f4f4f"];
+    const textWeight = 700;
+    console.log(data)
+
+    const createMeanSampleDiv = clusters => {
+      let currentChart = document.createElement("div")
+        currentChart = document.createElement("div")
+        currentChart.setAttribute("id", `meanSampleChart`);
+        currentChart.setAttribute("style", "width: 80%; vertical-align: top; margin-left:34%");
+        document.getElementById("charts").append(currentChart)
+      };
+
+    const drawMeanSamplePlots = clusters => {
+        var spoofInput = [{}];
+        spoofInput[0] = {...{}, values:data.processed.meanAllData};
+        visFactory("UncertaintyClusterSampleAndMean", {
+          chartElement:`meanSampleChart`, // ref.current,
+          data: spoofInput,
+          meanLineColors: colors,
+          divId: `meanSampleChart`,
+        }
+        );
+      
+    }
+    const createparameterRangeDiv = clusters => {
+      let currentChart = document.createElement("div")
+        currentChart = document.createElement("div")
+        currentChart.setAttribute("id", `parameterRangeChart`);
+        currentChart.setAttribute("style", "width: 34%; float: left");
+        document.getElementById("charts").append(currentChart)
+      };
+
+    const drawparameterRangePlot = clusters => {
+        var spoofInput = [{}];
+        spoofInput[0] = {...{}, values:data.processed.parameterData};
+        visFactory("SensitivityParameterRangeChart", {
+          chartElement:`parameterRangeChart`, // ref.current,
+          data: spoofInput,
+          colors: colors,
+          heightfactor: 1.05,
+          divId: `parameterRangeChart`,
+        }
+        );
+      
+    }
+    //Do not modify this order!
+    createparameterRangeDiv(data.processed)
+    drawparameterRangePlot(data.processed)
+    createMeanSampleDiv(data.processed)
+    drawMeanSamplePlots(data.processed)
+
+
+    //Redraw the plots when window size changes
+    function resize() {
+      createparameterRangeDiv(data.processed)
+      drawparameterRangePlot(data.processed)
+      createMeanSampleDiv(data.processed)
+      drawMeanSamplePlots(data.processed)
+      console.log("redrawing")
+    }
+    d3.select(window).on("resize.updatesvg", resize);
+  }
+}
--- src/lib/vis/uncertainty-cluster-sample-and-mean.js
+++ src/lib/vis/uncertainty-cluster-sample-and-mean.js
@@ -5,33 +5,40 @@ export class UncertaintyClusterSampleAndMean {
 
 
   constructor(options) {
-
     const data = options.data[0].values
-    let factor = 1 / 3.5
-    data.processed.length < 4 ? factor = 1 / data.processed.length : factor = 1 / 3.5
+    let factor = (data.processed.length < 6) ? 1 / data.processed.length : 1 / 5
     console.log(data)
+    let meanLineColors = options.meanLineColors ? options.meanLineColors : '';
+    let sampleLineColors = options.sampleLineColors ? options.sampleLineColors : '';
+    let chartWidths = options.chartWidths ? options.chartWidths : '';
+    let divId = options.divId ? options.divId : 'charts';
 
     const createDivs = clusters => {
       let currentChart = document.createElement("div")
       clusters.forEach((element, i) => {
         currentChart = document.createElement("div")
         currentChart.setAttribute("id", `chart${i}`);
-        document.getElementById("charts").append(currentChart)
+        currentChart.setAttribute("style", "width: " + chartWidths);
+        document.getElementById(divId).append(currentChart)
       });
     }
-
     const drawPlots = clusters => {
       clusters.forEach((element, i) => {
+        var spoofInput = [{}];
+        spoofInput[0] = { ...{}, values: element };
         visFactory("UncertaintySampleAndMean", {
           chartElement: `chart${i}`, // ref.current,
-          data: element,
-          heightfactor: factor
-
+          data: spoofInput,
+          heightfactor: factor,
+          widthfactor: 1,
+          meanLineColor: meanLineColors[i],
+          sampleLineColor: sampleLineColors[i],
+          containerChart: divId,
+          yMax: data.yMax
         }
         );
       });
     }
-
     createDivs(data.processed)
     drawPlots(data.processed)
 
