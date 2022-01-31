3b659ec1f043ca742f44afb3d9e46546502aca01
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-31 09:18:20 +0000

    Fix position of `/tools/gridded-glyphs` on small screens

--- src/pages/tools/gridded-glyphs/index.tsx
+++ src/pages/tools/gridded-glyphs/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import DashboardLayout from "src/components/dashboard-layout/DashboardLayout";
 import Head from "next/head";
 import DemographicsDashboard from "src/components/DemographicsDashboard";
+import { Box } from "@mui/material";
 
 const GriddedGlyphs = () => {
   return (
@@ -10,19 +11,18 @@ const GriddedGlyphs = () => {
         <title>Gridded glyphs</title>
       </Head>
       <DashboardLayout>
-        <div
-          // TODO: Fix styles for mobile (remove left 280 or change approach)
-          style={{
+        <Box
+          sx={{
             position: "fixed",
             overflow: "visible",
             bottom: 0,
-            left: 280,
+            left: { xs: 0, md: 280 },
             top: 60,
             right: 0,
           }}
         >
           <DemographicsDashboard />
-        </div>
+        </Box>
       </DashboardLayout>
     </>
   );
